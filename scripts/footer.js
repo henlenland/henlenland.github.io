8618a41b305de1e21c04caf1bf145c622db8c443
sourcy100 <dejdevatarovzena@gmail.com>
2025-06-08 02:39:41 +0500

    Inside Inital Commit

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+
+let ft = document.getElementsByTagName("footer")[0]
+
+ft.innerHTML = `
+<p><a href="///youtube.com/@Artswaigen">YouTube News</a>
+<a href="///t.me/@">Telegram</a>
+<a href="///github.com/sourcy100/Lengua">Lengua Github</a></p>
+<p>Sourcy site (C). All rights reserved.</p>`;
---
+++ scripts/header.js
@@ -0,0 +1,13 @@
+
+let hs = document.getElementsByTagName("header")[0]
+
+hs.innerHTML = `
+
+<a href="/">
+Dom
+</a>
+<nav>
+<a href="/lengua">
+Lengua la Transilvanja
+</a>
+</nav>`
