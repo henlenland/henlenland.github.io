e1448edca422b4ea56c27875a8f8f88e6ac7226a
Henlencyja <dejdevatarovzena@gmail.com>
2025-06-15 01:41:31 +0500

    big update

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -3,14 +3,14 @@ document.getElementsByTagName("sidebar")[0].innerHTML = `
 
 <a class="sec" href="/">Home</a>
 <a class="sec" href="/constitution">Constitution</a>
-<a class="sec" href="/parlament">Parlament</a>
-<a class="sec" href="/national-symbols">National Symbols</a>
+<a class="sec inner" href="/constitution/parlament">Parlament</a>
+<a class="sec inner" href="/constitution/symbols">Symbols</a>
 
 <div id="lang">
     <label>Language: </label>
     <select id="langch">
-        <option value="en" selected>en/US</option>
-        <option value="ru">ru/RU</option>
+        <option value="en" selected>🇬🇧</option>
+        <option value="ru">🇷🇺</option>
     </select>
 </div>
 `
