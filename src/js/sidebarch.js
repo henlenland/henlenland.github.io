e28c1017c5695338e9bcd371daa3b98cc59101ee
Henlencyja <dejdevatarovzena@gmail.com>
2025-06-15 20:33:19 +0500

    new site update

--- src/js/sidebarch.js
+++ src/js/sidebarch.js
@@ -13,9 +13,9 @@ function ch(zis){
     const ru = document.getElementById("ru")
     const banner = document.getElementById("banner")
     const footer = document.getElementsByTagName("footer")[0]
-    en.className = (en.className === ``) ? `sidebarsh` : ((en.className === `hidden`) ? `hidden` : ``)
-    ru.className = (ru.className === ``) ? `sidebarsh` : ((ru.className === `hidden`) ? `hidden` : ``)
-    banner.className = (banner.className === ``) ? `sidebarsh` : ``
+    en.className = (en.className === ``) ? `brightless` : ((en.className === `hidden`) ? `hidden` : ``)
+    ru.className = (ru.className === ``) ? `brightless` : ((ru.className === `hidden`) ? `hidden` : ``)
+    banner.className = (banner.className === ``) ? `brightless` : ``
     zis.className = (zis.className === ``) ? `sidebarsh` : ``
-    footer.className = (footer.className === ``) ? `sidebarsh` : ``
+    footer.className = (footer.className === ``) ? `brightless` : ``
 }
