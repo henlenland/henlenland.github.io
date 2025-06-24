cacbd4f119edbb8a6836cf752e62eae58ef03898
Henlencyja <dejdevatarovzena@gmail.com>
2025-06-24 22:28:13 +0500

    language

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -5,5 +5,7 @@ sidebar.innerHTML = `
 <a class="sec" href="/">Домашняя страница</a>
 <a class="sec" href="/lawbook">Ген. кодекс</a>
 <a class="sec" href="/newslist">Новости</a>
+<a class="sec" href="/language">Язык</a>
+<a class="sec inner" href="/language/dictionary">Словарь</a>
 
 `
