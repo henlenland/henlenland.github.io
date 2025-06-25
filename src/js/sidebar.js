e55135a06b41da7243bea2305d02ef51f69e802d
Henlencyja <dejdevatarovzena@gmail.com>
2025-06-25 11:29:02 +0500

    big dictature update

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -3,7 +3,7 @@ const sidebar = document.getElementsByTagName("sidebar")[0]
 sidebar.innerHTML = `
 
 <a class="sec" href="/">Домашняя страница</a>
-<a class="sec" href="/lawbook">Ген. кодекс</a>
+<a class="sec" href="/constitution">Конституция</a>
 <a class="sec" href="/newslist">Новости</a>
 <a class="sec" href="/language">Язык</a>
 <a class="sec inner" href="/language/dictionary">Словарь</a>`
