672febac1747e1f380214ff0d87c6661db16fa22
Русежениция <dejdevatarovzena@gmail.com>
2025-06-28 02:18:59 +0500

    bugfix xiiii

--- src/js/newslist.js
+++ src/js/newslist.js
@@ -2,7 +2,7 @@
 const lstnw = document.getElementById(`newslist`)
 
 fetch(`/news/app.json`).then(data => data.json()).then(ls => {
-    lstnw.innerHTML += `<li><a href="/newsview?v=latest">Новости за ${ls['newest']}</a></li>`
+    lstnw.innerHTML += `<li><a href="/newsview?v=newest">Новости за ${ls['newest']}</a></li>`
     for (let i=0;i<ls['other'].length;i++){
         lstnw.innerHTML += `<li><a href="/newsview?v=${ls['other'][i]}">Новости за ${ls['other'][i]}</a></li>`
     }
