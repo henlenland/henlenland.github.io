2952c0d6d4bf2a7e8ca2321e40be6287eb1b9cb5
Русежениция <dejdevatarovzena@gmail.com>
2025-07-06 18:52:36 +0500

    0607 upd3

--- hl/js/nav.js
+++ hl/js/nav.js
@@ -2,10 +2,10 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<p><a href="/en" style="font-size:25pt">Henlencia</a></p>
+<p><a href="/hl" style="font-size:25pt">Henlencia</a></p>
 
-<p><a href="/en/constitution">Konstitūßn</a></p>
-<p><a href="/en/newslist">Njūs</a><a href="/en/newsview?v=latest">Lāitest</a></p>
-<p><a href="/en/language">Lāngōeiʒ</a><a href="/en/language/keyboard">Kejbordenn</a></p>
+<p><a href="/hl/constitution">Konstitūßn</a></p>
+<p><a href="/hl/newslist">Njūs</a><a href="/hl/newsview?v=latest">Lāitest</a></p>
+<p><a href="/hl/language">Lāngōeiʒ</a><a href="/hl/language/keyboard">Kejbordenn</a></p>
 <p><a href="/">Tßusostern Lāngōeiʒ</a>
 `
