490967f68d5c6a030d24bc35faef44c5f3bd8bce
Русежениция <dejdevatarovzena@gmail.com>
2025-07-08 10:00:43 +0500

    Lengōeiʒenn

--- hl/js/nav.js
+++ hl/js/nav.js
@@ -4,8 +4,8 @@ sidebar.innerHTML = `
 
 <p><a href="/hl" style="font-size:25pt">Henlencia</a></p>
 
-<p><a href="/hl/constitution">Konstitūßn</a></p>
-<p><a href="/hl/newslist">Njūs</a><a href="/hl/newsview?v=latest">Lāitest</a></p>
+<p><a href="/hl/constitution">Konstitutsjon</a></p>
+<p><a href="/hl/newslist">Njuz</a><a href="/hl/newsview?v=latest">Lāitest</a></p>
 <p><a href="/hl/language">Lāngōeiʒ</a><a href="/hl/language/keyboard">Kejbordenn</a></p>
 <p><a href="/">Tßusostern Lāngōeiʒ</a>
 `
