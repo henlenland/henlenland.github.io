d19e73e6a0a128ba1a4c3264aa81a82e9e61541b
Русежениция <dejdevatarovzena@gmail.com>
2025-07-15 13:01:27 +0500

    1507-1

--- langChange.js
+++ langChange.js
@@ -2,8 +2,8 @@
 const form = document.getElementsByName(`lang`)[0]
 form.onchange = function (ev) {
 
-    let x = window.location.href
-
+    let x = window.location.pathname
+    
     if (x.includes('en')){
         x = x.replace('en', this.value);
     } else if (x.includes('ru')){
@@ -11,5 +11,6 @@ form.onchange = function (ev) {
     } else if (x.includes('hl')){
         x = x.replace('hl', this.value);
     }
+
     window.location.href = x
 }
