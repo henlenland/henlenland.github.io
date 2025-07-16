c73e45273cb13aaa5f06be79a0cfb2c243d789ef
Русежениция <dejdevatarovzena@gmail.com>
2025-07-17 03:20:52 +0500

    hl 1707-0

--- hl/js/nav.js
+++ hl/js/nav.js
@@ -2,12 +2,12 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<p><a href="/hl" style="font-size:25pt">Χενλενλανδε Ρεπυβλίκ</a></p>
+<p><a href="/hl" style="font-size:25pt">Henlenlánde Republíc</a></p>
 
-<p><a href="/hl/constitution">Κονστιτυτσηον</a><a href="/hl/newslist">Νηυζ</a><a href="/hl/newsview?v=latest">Λα̈ιτεστ</a><a href="/hl/language">Σπρεχ</a><a href="/hl/language/keyboard">Κεηβοαρδενν</a></p>
+<p><a href="/hl/constitution">Constitutßon</a><a href="/hl/newslist">Nyuez</a><a href="/hl/newsview?v=latest">Läitest</a><a href="/hl/language">Spreß</a><a href="/hl/language/keyboard">ßebordenn</a></p>
 <select name="lang">
     <option value="en">English</option>
     <option value="ru">Русский</option>
-    <option value="hl" selected>Χενλενε</option>
+    <option value="hl" selected>Henlene</option>
 </select>
 `
