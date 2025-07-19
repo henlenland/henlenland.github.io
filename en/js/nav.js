e04052b79046082bb8bf74e7a929bc2660be0148
Русежениция <dejdevatarovzena@gmail.com>
2025-07-19 17:25:53 +0500

    1907-1

--- en/js/nav.js
+++ en/js/nav.js
@@ -2,14 +2,12 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<p><a href="/en" style="font-size:25pt">Henlencia</a></p>
+<p><a href="/en" style="font-size:25pt">Henlenland</a></p>
 
 <p><a href="/en/constitution">Constitution</a></p>
 <p><a href="/en/newslist">News</a><a href="/en/newsview?v=latest">Latest</a></p>
-<p><a href="/en/language">Language</a><a href="/en/language/dictionary">Dictionary</a><a href="/en/language/keyboard">Keyboard</a></p>
 <select name="lang">
     <option value="en" selected>English</option>
     <option value="ru">Русский</option>
-    <option value="hl">Χενλενε</option>
 </select>
 `
--- ru/js/nav.js
+++ ru/js/nav.js
@@ -2,15 +2,12 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<p><a href="/ru" style="font-size:25pt">Хенленция</a></p>
+<p><a href="/ru" style="font-size:25pt">Хенленланд</a></p>
 
 <p><a href="/ru/constitution">Конституция</a></p>
 <p><a href="/ru/newslist">Новости</a><a href="/ru/newsview?v=latest">Новейшее</a></p>
-<p><a href="/ru/language">Язык</a><a href="/ru/language/dictionary">Словарь языка</a><a href="/ru/language/keyboard">Клавиатура</a></p>
-<p><a href="/">Выбор языка</a></p>
 <select name="lang">
     <option value="en">English</option>
     <option value="ru" selected>Русский</option>
-    <option value="hl">Χενλενε</option>
 </select>
 `
