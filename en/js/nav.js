8ec0a46f60077db2c2dfdf115d18aff066a804e0
Русежениция <dejdevatarovzena@gmail.com>
2025-07-27 19:29:38 +0500

    1807-1

--- en/js/nav.js
+++ en/js/nav.js
@@ -2,12 +2,12 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<a href="/en" style="font-size:30pt">Henlencia</a>
+<a href="/en" style="font-size:20pt">Henlencia</a>
 <a href="/en/laws#const">Constitution</a>
 <a href="/en/laws#lw">Laws</a>
 <a href="/en/newslist">News</a>
 <a href="/en/newsview?v=latest">Latest</a>
-
+<br />
 <select name="lang">
     <option value="en" selected>English</option>
     <option value="ru">Русский</option>
--- ru/js/nav.js
+++ ru/js/nav.js
@@ -2,13 +2,13 @@
 const sidebar = document.getElementsByTagName("nav")[0]
 sidebar.innerHTML = `
 
-<p><a href="/ru" style="font-size:30pt">Хенленция</a></p>
+<p><a href="/ru" style="font-size:20pt">Хенленция</a></p>
 
 <a href="/ru/laws#const">Конституция</a>
 <a href="/ru/laws#lw">Законы</a>
 <a href="/ru/newslist">Новости</a>
 <a href="/ru/newsview?v=latest">Новейшее</a>
-
+<br />
 <select name="lang">
     <option value="en">English</option>
     <option value="ru" selected>Русский</option>
