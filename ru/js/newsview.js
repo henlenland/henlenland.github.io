e4316e9581ecceddf45cf1e259d7cd9bf3bc6a08
Русежениция <dejdevatarovzena@gmail.com>
2025-08-15 17:59:19 +0500

    rustest-2 1508-1

--- ru/js/newsview.js
+++ ru/js/newsview.js
@@ -1,27 +1,25 @@
 
-function newsv(path){
-    fetch(`/ru/news/${path}.html`)
-    .then(result => result.text())
-    .then(data => {
-        return data
-    }).catch(err => {return `error ${err}`})
+async function newsv(path){
+    let response = await fetch(`/ru/news/${path}.html`)
+    let data = await response.text()
+    return data
 }
 
 const main = document.getElementsByTagName('main')[0]
 
-fetch(`/news.json`).then(data => data.json()).then(ls => {
-    for (let i = 0; i < ls.length; i++){
-
-        let it = ls[i]
+fetch(`/news.json`).then(data => data.json()).then(dat => {
+    for (const it of dat){
         let date = 
             it.substring(0, 2) + '.' + 
             it.substring(2, 4) + '.' + 
             it.substring(4, 6)
 
-        main.innerHTML += `
-        <h2>${date}</h1>
-        ${newsv(it)}
-        <hr />
-        `
+        newsv(it).then(data => {
+            main.innerHTML += `
+            <h2>${date}</h1>
+            ${data}
+            <hr />
+            `
+        })
     }
 })
