38472053f543a7509fa8fd268f260cc5d8c18657
Русежениция <dejdevatarovzena@gmail.com>
2025-08-17 11:21:33 +0500

    1708-6

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,8 +3,18 @@ const nav = document.getElementsByTagName("nav")[0]
 nav.innerHTML = `
 
 <a href="/">Главная страница</a>
-<a href="/laws">Конституция</a>
+<a href="/constitution">Конституция</a>
 <a href="/news">Новости</a>
+
+`
+
+
+const footer = document.getElementsByTagName("footer")[0]
+footer.innerHTML = `
+
 <a href="https://t.me/henlenland">Telegram-канал</a>
-<a href="https://t.me/henlenland">Telegram-чат</a>
+<a href="https://t.me/henlenlande_chat">Telegram-чат</a>
+<br />
+Сайт Хенленции © - 2025
+
 `
