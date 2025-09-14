d1baeaeed755773e7c7c13e5aa251ee787dd1295
Henlenland <dejdevatarovzena@gmail.com>
2025-09-14 18:10:37 +0500

    1409-7

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,41 @@
 
 const nav = document.getElementsByTagName("nav")[0]
-if (nav.clientWidth > 650){
+const footer = document.getElementsByTagName("footer")[0]
+
+if (!window.matchMedia('(max-width: 650px)').matches){
+
+    footer.innerHTML = `
+    
+    <table>
+    
+        <tr>
+    
+            <td>
+                <p><a href="https://t.me/henlenland">Telegram-канал</a></p>
+                <p><a href="https://t.me/henlenlande_chat">Telegram-чат</a></p>
+            </td>
+    
+            <td>
+                <p>Контактная информация:</p>
+                <p>@Sourcy1000 в Telegram</p>
+                <p>@sourcy100 в Discord</p>
+            </td>
+    
+        </tr>
+    
+        <tr>
+    
+            <td colspan="2">
+                <hr>
+                <p>© 06.2025 - 09.2025 Хенленция</p>
+            </td>
+    
+        </tr>
+    
+    </table>
+    `;
+
+
     nav.innerHTML = `
 
     <table>
@@ -27,6 +62,32 @@ if (nav.clientWidth > 650){
     </table>
     `;
 } else {
+    footer.innerHTML = `
+    
+    <table>
+    
+        <tr>
+            <td>
+                <p><a href="https://t.me/henlenland">Telegram-канал</a></p>
+                <p><a href="https://t.me/henlenlande_chat">Telegram-чат</a></p>
+                
+                <p>Контактная информация:</p>
+                <p>@Sourcy1000 в Telegram</p>
+                <p>@sourcy100 в Discord</p>
+            </td>
+        </tr>
+        <tr>
+    
+            <td>
+                <hr>
+                <p>© 06.2025 - 09.2025 Хенленция</p>
+            </td>
+    
+        </tr>
+    
+    </table>
+    `;
+
     nav.innerHTML = `
 
     <table>
@@ -51,35 +112,3 @@ if (nav.clientWidth > 650){
     </table>
     `;
 }
-
-const footer = document.getElementsByTagName("footer")[0]
-footer.innerHTML = `
-
-<table>
-
-    <tr>
-
-        <td>
-            <p><a href="https://t.me/henlenland">Telegram-канал</a></p>
-            <p><a href="https://t.me/henlenlande_chat">Telegram-чат</a></p>
-        </td>
-
-        <td>
-            <p>Контактная информация:</p>
-            <p>@Sourcy1000 в Telegram</p>
-            <p>@sourcy100 в Discord</p>
-        </td>
-
-    </tr>
-
-    <tr>
-
-        <td colspan="2">
-            <hr>
-            <p>© 06.2025 - 09.2025 Хенленция</p>
-        </td>
-
-    </tr>
-
-</table>
-`
