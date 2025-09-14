61558644208087848215954e1bba80b1f7400be6
Henlenland <dejdevatarovzena@gmail.com>
2025-09-14 09:09:29 +0500

    1409-1

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,15 +3,12 @@ const nav = document.getElementsByTagName("nav")[0]
 nav.innerHTML = `
 
 <p>
-    <a href="/" style="font-size: 20pt">Хенленция</a>
+    <a href="/" style="font-size: 18pt">Хенленция</a>
     |
     <a href="/executive_orders">Указы</a>
     <a href="/constitution">Конституция</a>
+    <!-- <a href="/fun">Развлечения</a>-->
     <a href="/news">Новости</a>
-</p>
-<p><a href="#" style="font-size: 20pt">Развлечения</a>
-|
-<a href="/constitution_old_russian">(старославянск.) Ѹставъ</a>
 </p>
 `
 
@@ -19,9 +16,31 @@ nav.innerHTML = `
 const footer = document.getElementsByTagName("footer")[0]
 footer.innerHTML = `
 
-<a href="https://t.me/henlenland">Telegram-канал</a>
-<a href="https://t.me/henlenlande_chat">Telegram-чат</a>
-<br />
-© 06.2025 - 09.2025 Хенленция
+<table width="100%">
 
+    <tr>
+
+        <td>
+            <p><a href="https://t.me/henlenland">Telegram-канал</a></p>
+            <p><a href="https://t.me/henlenlande_chat">Telegram-чат</a></p>
+        </td>
+
+        <td>
+            <p>Контактная информация:</p>
+            <p>@Sourcy1000 в Telegram</p>
+            <p>@sourcy100 в Discord</p>
+        </td>
+
+    </tr>
+
+    <tr>
+
+        <td colspan="2">
+            <hr>
+            <p>© 06.2025 - 09.2025 Хенленция</p>
+        </td>
+
+    </tr>
+
+</table>
 `
