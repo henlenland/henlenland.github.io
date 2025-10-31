39c18bec1b7371300ec9162ddb64ac3dfb785fd6
Henlenland <eugenesourcy@gmail.com>
2025-11-01 04:42:58 +0500

    0111-0

--- src/js/articles.js
+++ src/js/articles.js
@@ -2,11 +2,13 @@ const main = document.getElementsByTagName('main')[0]
 
 
 fetch('/wiki/articles.json').then(response => response.json()).then(json => {
-    let s = `<h1>Посмотрите эти статьи:</h1>`;
-    json.forEach(r => {
-        s += `<h3><li><a href="/wiki/${r}">Статья &laquo;${r}&raquo;</a></li></h3>`;
+    let s = `<h2>Посмотрите эти статьи:</h2>`;
+    let keys = Object.keys(json)
+    keys.forEach(key => {
+        s += `<h3><li>Статья <a href="/wiki/${key}">&laquo;${json[key]}&raquo;</a></li></h3>`;
     });
-    main.innerHTML = s;
+    main.innerHTML += s;
 }).catch(a => {
-    s = "error";
+    s = `error ${a}`;
+    main.innerHTML += s;
 });
