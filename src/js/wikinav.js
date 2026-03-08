fbc35729d320e74cd919457681a1073d01b4588a
Henlenland <eugenesourcy@gmail.com>
2026-03-09 02:22:29 +0500

    0903-0

--- src/js/wikinav.js
+++ src/js/wikinav.js
@@ -3,6 +3,7 @@ const header = document.getElementsByTagName("header")[0]
 
 header.innerHTML += `
     <a href="/" class="el"><b>ХенленВики</b></a>
+    <a class="el">Powered by SourcyWiki (C)</a>
     <span style="float: right;" class="el">
         <label for="searchText">Поиск: </label>
         <input type="text" id="searchText" onChange="window.location.href = \`/search?id=\$\{document.getElementById('searchText').value\}\`">
